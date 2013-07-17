ea8c9511e3a6d95bf246b7e5c71676354dceb901
Edevwarrior <edevwarrior@yahoo.com>
2013-07-17 22:29:17 +0200

    fs

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  	var indexBuffer = fs.readFileSync('index.html');
+	var indexContent = indexBuffer.toString('utf-8');
+
+	response.send(indexContent);
 });
 
 var port = process.env.PORT || 5000;
